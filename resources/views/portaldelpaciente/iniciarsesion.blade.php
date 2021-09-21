28edd3ba57a88cf8cacd59eb429a311fa4ebb71b
leoocaba <leoo.caballero@gmail.com>
2021-09-21 18:43:14 -0300

    Vista 'Login-Register'.

--- resources/views/portaldelpaciente/iniciarsesion.blade.php
+++ resources/views/portaldelpaciente/iniciarsesion.blade.php
@@ -2,155 +2,81 @@
 
 @section('css')
 
-            {{-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'> --}}
-            {{-- <link rel="stylesheet" href="{{ asset('css/login.css') }}"> --}}
+{{-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'> --}}
+<link rel="stylesheet" href="{{ asset('css/login-register.css') }}">
 <style>
-
-/* .input {
-  width: 100%;
-  padding: 10px;
-  background: transparent;
-  border: none;
-  outline: none;
-} */
-/* *{
-  margin: 0;
-  padding: 0;
-  font-family: sans-serif;
-} */
-
-/* #container{
-  width: 800px;
-  height: 500px;
-  background: inherit;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin-top: -250px;
-  margin-left: -400px;
-  overflow: hidden;
-} */
-
-/* #container:before{
-  content: "";
-  background: inherit;
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  box-shadow: inset 0 0 50px rgba(255,255,255, 0.3);
-  filter: blur(8px);
-} */
-/* #back-btn{
-  position: absolute;
-  background: white;
-  width: 100px;
-  height: 100%;
-} */
-/* #form{
-  position: absolute;
-  right: 0;
-} */
-#sign-in, #sign-up{
-  display: inline-block;
-  text-align: center;
-  position: relative;
-}
-#sign-in{
-  width: 350px;
-  height: 520px;
-  box-shadow: inset 0 0 0 3000px rgba(165, 193, 245, 0.842);
-  /* margin-right: -4px; */
-}
-#sign-up{
-  width: 350px;
-  height: 520px;
-  box-shadow: inset 0 0 0 3000px rgba(165, 193, 245, 0.842);
-}
-
-#form-content{
-  text-align: center;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%,-50%);
-}
-input{
-  background: 0;
-  width: 200px;
-  outline: 0;
-  border: 0;
-  border-bottom: 2px solid rgba(14, 13, 13, 0.103);
-  margin: 15px 0;
-  padding-bottom: 10px;
-  font-size: 14px;
-  font-weight: bold;
-  color: rgba(4, 3, 75, 0.8);
-}
-input[type="submit"]{
-  border: 0;
-  border-radius: 8px;
-  padding-bottom: 0;
-  height: 60px;
-  background: rgba(4, 3, 75, 0.8);
-  color: white;
-  cursor: pointer;
-  transition: all 600ms ease-in-out;
-}
-::placeholder{
-  color: rgb(15, 75, 153);
-  font-size: 14px;
-}
-input[type="submit"]:hover{
-  background: #C0392B;
-}
-span a{
-  color: rgba(255,255,255, 0.8);
-}
 
 </style>
 @endsection
 
 @section('content')
 
-<article class="container mx-auto p-0">
-{{-- <div class="container"> --}}
-
-    <div class="row justify-content-center p-1">
-        <div class="col-md-4 col-sm-2 col-sm-offset-1">
-            <div class="col-md-12">
-                <h2 style="color: rgba(4, 3, 75, 0.8);font-size: .8em"><b>Si ya se registró, ingrese su email y contraseña</b></h2>
-                <form id="sign-in"action="">
-                    <div id="form-content">
-                        <h2 style="color: rgba(4, 3, 75, 0.8);"><b>Ingresar</b></h2>
-                        <input type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" size="30" required placeholder="Correo electronico"><br>
-                        <input type="password" placeholder="Contraseña"><br>
-                        <input type="submit" value="INGRESAR"><br>
-                        <span><a href="#">Se olvido la contraseña?</a></span>
-                    </div>
-                </form>
+<section>
+  <article class="mx-auto px-0">
+    <div class="row mx-0 px-0">
+        <img class="img-fluid px-0" src={{ asset("images/img/banner-portaldelpaciente.png")}} alt="Imagen de portada de Portal del Paciente">
+        <div class="d-flex flex-column m-0 p-0">
+            <div class="__titulo-seccion-portaldelpaciente col-12 col-sm-12 col-md-6 col-lg-4 d-block d-flex justify-content-center justify-content-sm-center justify-content-md-end justify-content-lg-end my-1">
+                <h5  class="__titulo fs-5 text-uppercase text-white pl-4 py-1 my-auto">Portal del paciente</h5>
             </div>
-        </div>
-        <div class="col-md-4 col-sm-2 col-sm-offset-1">
-            <div class="col-md-12">
-                <h2 style="color: rgba(4, 3, 75, 0.8);font-size: .8em"><b>Para registrarse, complete los campos que aparecen a continuación</b></h2>
-                <form id="sign-up" action="">
-                    <div id="form-content">
-                        <h2 style="color: rgba(4, 3, 75, 0.8);"><b>Registrarse</b></h2>
-                        <input type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" size="30" required placeholder="Nombre y apellido"><br>
-                        <input type="text" placeholder="Correo electronico"><br>
-                        <input type="number" placeholder="Telefono"><br>
-                        <input type="number" placeholder="D.N.I"><br>
-                        <input type="password" placeholder="Contraseña"><br>
-                        <input type="submit" value="REGISTRARSE"><br>
-                        {{-- <span><a href="#">ya tiene una cuenta?</a></span> --}}
-                    </div>
-                </form>
-            </div>    
         </div>
     </div>
 </article>
+
+<article class="container mx-auto p-0 my-4">
+  <div class="row justify-content-center p-1">
+      <div class="col-10 d-flex flex-column flex-sm-column flex-md-row flex-lg-row justify-content-center">
+        <div class="col-10 col-sm-10 col-md-6 col-lg-5 mx-lg-4 d-flex justify-content-center mx-auto mx-sm-auto mx-md-4 mx-lg-4">
+          <div class="col-12 justify-content-center mx-auto">
+              <h6 class="__title-form col-12 text-center text-uppercase fw-bolder">Si ya se registró, ingrese su email y contraseña</h6>
+              <form class="__form container my-3 py-4">
+                <div class="col-12 d-flex justify-content-start my-2">
+                  <h4 class="fw-bolder">Ingresar</h4>
+                </div>
+                <div class="mb-3">
+                  <input type="text" class="__input form-control border-0 border-bottom rounded-0" id="exampleInputName1" placeholder="Nombre y Apellido" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <input type="email" class="__input form-control border-0 border-bottom rounded-0" id="exampleInputEmail1" placeholder="Correo electrónico" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <input type="password" class="__input form-control border-0 border-bottom rounded-0" placeholder="Contraseña" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="__btn-submit col-12 btn btn-primary btn-block rounded-0">Ingresar</button>
+              </form>
+          </div>
+      </div>
+      <div class="col-10 col-sm-10 col-md-6 col-lg-5 mx-lg-4 d-flex justify-content-center mx-auto mx-sm-auto mx-md-4 mx-lg-4">
+          <div class="col-12 justify-content-center mx-auto">
+              <h6 class="__title-form col-12 text-center text-uppercase fw-bolder">Para registrarse, complete los siguientes campos</h6>
+              <form class="__form container my-3 py-4">
+                <div class="col-12 d-flex justify-content-start my-2">
+                  <h4 class="fw-bolder">Registrarse</h4>
+                </div>
+                <div class="mb-3">
+                  <input type="text" class="__input form-control border-0 border-bottom rounded-0" id="exampleInputName1" placeholder="Nombre y Apellido" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <input type="email" class="__input form-control border-0 border-bottom rounded-0" id="exampleInputEmail1" placeholder="Correo electrónico" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <input type="text" class="__input form-control border-0 border-bottom rounded-0" id="exampleInputTel1" placeholder="Teléfono" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <input type="text" class="__input form-control border-0 border-bottom rounded-0" id="exampleInputDNI1" placeholder="DNI" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <input type="password" class="__input form-control border-0 border-bottom rounded-0" placeholder="Contraseña" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="__btn-submit col-12 btn btn-primary btn-block rounded-0">Registrarse</button>
+              </form>
+          </div>    
+      </div>
+      </div>
+  </div>
+</article>
+
+</section>
 
 @endsection
 
